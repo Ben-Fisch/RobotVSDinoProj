a5ef90e4081612e622d9f213d17781687b62db5e
Ben Fischer <fischerben010@gmail.com>
2021-10-06 14:20:24 -0500

    I have added three objects for both the Robots and Dinosaurs of which will be added to the lists on Herd and Fleet.

--- herd.py
+++ herd.py
@@ -1,6 +1,14 @@
+from dinosaur import Dinosaur
+
+
 class Herd:
     def __init__(self):
         self.dinosaurs = []
 
     def create_herd(self):
-        pass
+        dinosaur_one = Dinosaur('Frank', 5)
+        dinosaur_two = Dinosaur('Lori', 10)
+        dinosaur_three = Dinosaur('Dante', 20)
+        self.dinosaurs.append[dinosaur_one]
+        self.dinosaurs.append(dinosaur_two)
+        self.dinosaurs.append(dinosaur_three)
